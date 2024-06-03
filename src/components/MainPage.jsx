905f02215844565c7c971a688855e3b48597b0e8
SaraGarciaM <saragarciamatos@gmail.com>
2024-06-03 21:17:58 +0100

    Sets up basic structure with some responsiveness

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 function MainPage() {
   return (
-    <div>
-      <nav className="flex w-2/12 m-4">
+    <div className="w-screen">
+      <nav className="flex m-4 xl:ml-5 font-poppins text-base">
         <Link
           to="https://www.linkedin.com/in/saragmatos"
           target="_blank"
@@ -14,24 +14,30 @@ function MainPage() {
         <Link
           to="https://www.github.com/SaraGMatos"
           target="_blank"
-          className="m-2 mr-5 ml-5 mb-10"
+          className="m-2 mr-5 ml-5"
         >
           GitHub
         </Link>
       </nav>
-      <section>
-        <h1>Sara Matos</h1>
-        <h2>Junior Software Developer</h2>
-        <p>Working on personal projects and volunteering.</p>
-        <p>Actively looking for a dev role within the UK.</p>
-      </section>
-      <nav className="main-nav">
-        <Link>SKILLS</Link>
-        <Link>PROJECTS</Link>
-        <Link>ABOUT</Link>
-        <Link>CONTACT</Link>
-      </nav>
-      <section className="main-page-description"></section>
+      <div className="flex flex-wrap w-screen">
+        <section className="m-10 mt-0 mb-0 xl:ml-15 grow self-center font-poppins">
+          <h1 className="font-bold mb-3 text-xl">SARA MATOS</h1>
+          <h2 className="mb-3 text-lg">Junior Software Developer</h2>
+          <p className="mb-3 text-lg">
+            Working on personal projects and volunteering
+          </p>
+          <p className="mb-3 text-lg">
+            Actively looking for a dev role within the UK
+          </p>
+        </section>
+        <nav className="flex flex-col m-10 mt-1 grow font-antic text-[80px] sm:text-9xl md:text-[160px] lg:text-[180px] xl:text-[180px] 2xl:text-[160px] xl:mr-20 max-w-fit">
+          <Link className="xl:m-3">SKILLS</Link>
+          <Link className="xl:m-3">PROJECTS</Link>
+          <Link className="xl:m-3">ABOUT</Link>
+          <Link className="xl:m-3">CONTACT</Link>
+        </nav>
+        <section className="main-page-description"></section>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        antic: ["Antic Didone", "serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
