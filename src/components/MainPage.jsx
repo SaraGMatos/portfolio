3cd2e896c20951c024c54fa9bcad8dc9402a0407
SaraGarciaM <saragarciamatos@gmail.com>
2024-06-03 22:04:25 +0100

    Adds some more styles to main page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -7,34 +7,51 @@ function MainPage() {
         <Link
           to="https://www.linkedin.com/in/saragmatos"
           target="_blank"
-          className="m-2 mr-5 ml-5"
+          className="m-2 mr-5 ml-5 text-huntergreen font-semibold"
         >
           LinkedIn
         </Link>
         <Link
           to="https://www.github.com/SaraGMatos"
           target="_blank"
-          className="m-2 mr-5 ml-5"
+          className="m-2 mr-5 ml-5 text-huntergreen font-semibold"
         >
           GitHub
         </Link>
+        <hr
+          width="120px"
+          size="30px"
+          className="ml-6 self-center h-1 border-huntergreen"
+        ></hr>
       </nav>
       <div className="flex flex-wrap w-screen">
         <section className="m-10 mt-0 mb-0 xl:ml-15 grow self-center font-poppins">
-          <h1 className="font-bold mb-3 text-xl">SARA MATOS</h1>
-          <h2 className="mb-3 text-lg">Junior Software Developer</h2>
-          <p className="mb-3 text-lg">
+          <h1 className="font-bold mb-3 text-3xl text-huntergreen">
+            SARA MATOS
+          </h1>
+          <h2 className="mb-3 text-lg text-huntergreen">
+            Junior Software Developer
+          </h2>
+          <p className="mb-3 text-lg text-huntergreen">
             Working on personal projects and volunteering
           </p>
-          <p className="mb-3 text-lg">
+          <p className="mb-3 text-lg text-huntergreen">
             Actively looking for a dev role within the UK
           </p>
         </section>
         <nav className="flex flex-col m-10 mt-1 grow font-antic text-[80px] sm:text-9xl md:text-[160px] lg:text-[180px] xl:text-[180px] 2xl:text-[160px] xl:mr-20 max-w-fit">
-          <Link className="xl:m-3">SKILLS</Link>
-          <Link className="xl:m-3">PROJECTS</Link>
-          <Link className="xl:m-3">ABOUT</Link>
-          <Link className="xl:m-3">CONTACT</Link>
+          <Link className="xl:m-3 text-ferngreen hover:italic hover:translate-x-4 hover:transition-transform ease-in duration-200">
+            SKILLS
+          </Link>
+          <Link className="xl:m-3 text-ferngreen hover:italic hover:translate-x-4 hover:transition-transform ease-in duration-200">
+            PROJECTS
+          </Link>
+          <Link className="xl:m-3 text-ferngreen hover:italic hover:translate-x-4 hover:transition-transform ease-in duration-200">
+            ABOUT
+          </Link>
+          <Link className="xl:m-3 text-ferngreen hover:italic hover:translate-x-4 hover:transition-transform ease-in duration-200">
+            CONTACT
+          </Link>
         </nav>
         <section className="main-page-description"></section>
       </div>
