aaea900d1bdc658169bd4cf9adcfd1148685c85f
SaraGarciaM <saragarciamatos@gmail.com>
2024-06-05 22:22:49 +0100

    Basic pair and team work content

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { IoLogoJavascript } from "react-icons/io5";
 
 function MainPage() {
   return (
@@ -66,7 +65,7 @@ function MainPage() {
           </nav>
         </div>
 
-        <section id="skills" className="bg-pakistangreen w-full h-full p-9">
+        <section id="skills" className="bg-pakistangreen w-full p-9">
           <h2 className="mb-5 sm:mb-8 text-teagreen font-antic text-[80px] sm:text-9xl md:text-[160px] lg:text-[160px] xl:text-[140px] 2xl:text-[140px] xl:mr-20">
             SKILLS
           </h2>
@@ -89,71 +88,119 @@ function MainPage() {
           </div>
           <div
             id="skills-content"
-            className="flex flex-wrap justify-around mt-12"
+            className="flex flex-col flex-wrap justify-around gap-11 mt-12"
           >
-            <section id="frontend" className="flex flex-col text-center w-80">
-              <h3 className="text-earthyellow font-poppins text-2xl sm:text-3xl">
-                front end
-              </h3>
-              <p className="text-teagreen text-justify">
-                Planning and designing smart interfaces using visual tools like{" "}
-                <b>Figma</b> and <b>Excalidraw</b>.
-              </p>
-              <p className="text-teagreen text-justify">
-                Creating user-friendly interfaces with web technologies such as
-                <b> JavaScript</b>, <b>HTML</b>, <b>CSS</b> and the much loved{" "}
-                <b>ReactJS</b>.
-              </p>
-              <p className="text-teagreen text-justify">
-                Using <b>Axios</b> as HTTP client for making API requests from
-                the client side.
-              </p>
-              <p className="text-teagreen text-justify">
-                Adding some colours and sparkles using popular framaworks like{" "}
-                <b>TailwindCSS</b> or <b>Bootstrap</b>.
-              </p>
-            </section>
-            <section id="backend" className="flex flex-col text-center w-80">
-              <h3 className="text-earthyellow font-poppins text-2xl sm:text-3xl">
-                back end
-              </h3>
-              <p className="text-teagreen text-justify">
-                Building solid RESTful APIs using <b>Node.js</b> and{" "}
-                <b>ExpressJS</b>.
-              </p>
-              <p className="text-teagreen text-justify">
-                Creating, seeding and querying <b>PostgreSQL</b> databases using
-                both <b>psql</b> and <b>Azure Data Studio</b>.
-              </p>
-              <p className="text-teagreen text-justify">
-                Using <b>Supabase</b> as backend service to quickly build robust
-                apps.
-              </p>
-              <p className="text-teagreen text-justify">
-                Exploring the world of OOP using <b>C#</b> and <b>Java</b>.
-              </p>
-            </section>
-            <section id="testing" className="flex flex-col text-center w-80">
-              <h3 className="text-earthyellow font-poppins text-2xl sm:text-3xl">
-                testing
-              </h3>
-              <p className="text-teagreen text-justify">
-                Ensuring code efficiency and reliability through{" "}
-                <b>test driven development</b>.
-              </p>
-              <p className="text-teagreen text-justify">
-                Writing robust unit tests with <b>Jest</b>.
-              </p>
-              <p className="text-teagreen text-justify">
-                Safeguarding API call reliability through integration testing
-                with <b>Supertest</b>.
-              </p>
-            </section>
-            <section id="teamwork">
-              <h3 className="text-earthyellow font-poppins text-2xl sm:text-3xl">
-                team work
-              </h3>
-            </section>
+            <div
+              id="section-one"
+              className="flex flex-wrap justify-around gap-5"
+            >
+              <section id="frontend" className="flex flex-col text-center w-80">
+                <h3 className="text-earthyellow font-poppins text-2xl sm:text-3xl">
+                  front end
+                </h3>
+                <p className="text-teagreen text-justify">
+                  Planning and designing smart interfaces using visual tools
+                  like <b>Figma</b> and <b>Excalidraw</b>.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Creating user-friendly interfaces with web technologies such
+                  as
+                  <b> JavaScript</b>, <b>HTML</b>, <b>CSS</b> and the much loved{" "}
+                  <b>ReactJS</b>.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Using <b>Axios</b> as HTTP client for making API requests from
+                  the client side.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Adding some colours and sparkles using popular frameworks like{" "}
+                  <b>TailwindCSS</b> or <b>Bootstrap</b>.
+                </p>
+              </section>
+              <section id="backend" className="flex flex-col text-center w-80">
+                <h3 className="text-earthyellow font-poppins text-2xl sm:text-3xl">
+                  back end
+                </h3>
+                <p className="text-teagreen text-justify">
+                  Building solid RESTful APIs using <b>Node.js</b> and{" "}
+                  <b>ExpressJS</b>.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Creating, seeding and querying <b>PostgreSQL</b> databases
+                  using both <b>psql</b> and <b>Azure Data Studio</b>.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Using <b>Supabase</b> as backend service to quickly build
+                  robust apps.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Exploring the world of OOP using <b>C#</b> and <b>Java</b>.
+                </p>
+              </section>
+              <section id="testing" className="flex flex-col text-center w-80">
+                <h3 className="text-earthyellow font-poppins text-2xl sm:text-3xl">
+                  testing
+                </h3>
+                <p className="text-teagreen text-justify">
+                  Ensuring code efficiency and reliability through{" "}
+                  <b>test driven development</b>.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Writing robust unit tests with <b>Jest</b>.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Safeguarding API call reliability through integration testing
+                  with <b>Supertest</b>.
+                </p>
+              </section>
+            </div>
+
+            <div
+              id="section-two"
+              className="flex flex-wrap justify-around gap-5"
+            >
+              <section id="teamwork" className="flex flex-col text-center w-80">
+                <h3 className="text-earthyellow font-poppins text-2xl sm:text-3xl">
+                  pair & team work
+                </h3>
+                <p className="text-teagreen text-justify">
+                  Fostering an efficient flow of work and communication within a
+                  pair/team.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Adhering to <b>Agile practices</b> like <b>Scrum</b>.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Leading daily stand-ups.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Utilising <b>Kanban boards</b> for visualising and managing
+                  work.
+                </p>
+              </section>
+              <section
+                id="other-technologies"
+                className="flex flex-col text-center w-80"
+              >
+                <h3 className="text-earthyellow font-poppins text-2xl sm:text-3xl">
+                  other technologies
+                </h3>
+                <p className="text-teagreen text-justify">
+                  <b>Git</b> as version control.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Employing some popular hosting services like <b>Netlify</b>,{" "}
+                  <b>Render</b> or <b>Railway</b>.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Implementing live chat functionality with <b>Socket.IO</b>.
+                </p>
+                <p className="text-teagreen text-justify">
+                  Using <b>Inquirer</b> to create simple command line
+                  applications.
+                </p>
+              </section>
+            </div>
           </div>
         </section>
       </div>
