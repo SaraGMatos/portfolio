602b5bc30f27d5163b236022adcf47bdefc814ef
SaraGarciaM <saragarciamatos@gmail.com>
2024-06-14 16:01:11 +0100

    Tweaks some things

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
 import MainPage from "./components/MainPage";
-import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-    </Routes>
+    <>
+      <MainPage />;
+    </>
   );
 }
 
